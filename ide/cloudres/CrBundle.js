278ae07e39f0594c6c2d533a3c7193839d627630
Jimmy Xu <jimmy.xu@madeiracloud.com>
2014-07-08 10:50:36 +0800

    pre-3.19.1049

--- ide/cloudres/CrBundle.js
+++ ide/cloudres/CrBundle.js
@@ -2478,11 +2478,14 @@
         });
       },
       parseFetchData: function(data) {
-        var ami, amiIds, id, savedAmis;
+        var ami, amiIds, id, savedAmis, _ref;
         savedAmis = [];
         amiIds = [];
         for (id in data) {
           ami = data[id];
+          if (ami.architecture === 'i386' || (ami.name.indexOf('by VisualOps') === -1 && ((_ref = ami.osType) !== 'windows' && _ref !== 'suse'))) {
+            continue;
+          }
           ami.id = id;
           savedAmis.push(ami);
           amiIds.push(id);
