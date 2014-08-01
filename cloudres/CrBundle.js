785bbfe35224c85d53a4f7c38e5704ca0410b1d1
song <song@mc2.io>
2014-08-01 12:15:17 +0800

    pre-3.34.1214

--- cloudres/CrBundle.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  define(["CloudResources", "./CrClnSharedRes", "./CrClnCommonRes", "./CrClnOpsResource", "./CrClnAmi", "./CloudImportVpc", "./CrClnRds", "./CrClnRdsParam"], function(CloudResources) {
-    return CloudResources;
-  });
-
-}).call(this);
--- workspaces/editor/canvas/CanvasBundle.js
+++
@@ -1,6 +0,0 @@
-(function() {
-  define(["./CanvasView", "./CanvasViewDnd", "./CanvasViewConnect", "./CanvasViewGResizer", "./CanvasManager", "./CeVpc", "./CeAz", "./CeSubnet", "./CeRtb", "./CeIgw", "./CeVgw", "./CeCgw", "./CeElb", "./CeEni", "./CeInstance", "./CeAsg", "./CeLc", "./CeSgAsso", "./CeLine", "./CeSgLine", "./CeDbInstance", "./CeDbSubnetGroup"], function(CanvasView) {
-    return CanvasView;
-  });
-
-}).call(this);
