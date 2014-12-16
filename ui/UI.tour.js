9e9d7e6d3e4fc0bdd773d4bd474b3a6000c1d281
Jimmy Xu <jimmy.xu@madeiracloud.com>
2014-12-16 08:34:02 +0800

    pre-3.97.832

--- ui/UI.tour.js
+++ ui/UI.tour.js
@@ -9,41 +9,4 @@
 # **********************************************************
 */
 
-define(['jquery'], function($) {
-
-	$.fn.showTour = function(options) {
-		$target = $(this);
-		$tourBox = $(
-			'<div class="user-tour">\
-				<span class="user-tour-title">This is title</span>\
-				<div class="user-tour-pointer"></div>\
-				<div class="user-tour-pointer animation"></div>\
-			</div>').appendTo('body');
-
-		$tourPointer = $tourBox.children('.user-tour-pointer');
-
-		var tourPos = {};
-		var tourPointerPos = {};
-
-		targetOffset = $target.offset();
-		targetWidth = $target.innerWidth();
-		targetHeight = $target.innerHeight();
-
-		tourWidth = $tourBox.width();
-		tourHeight = $tourBox.height();
-
-		if (targetOffset.left + targetWidth + tourWidth - document.documentElement.scrollLeft > window.innerWidth) {
-			tourPos.left = targetOffset.left - tourWidth - 15;
-			tourPointerPos['margin-left'] = tourWidth + 5;
-		} else {
-			tourPos.left = targetOffset.left + targetWidth + 15;
-			tourPointerPos['margin-left'] = -15;
-		}
-
-		tourPos.top = targetOffset.top - ((tourHeight - targetHeight) / 2);
-		tourPointerPos['margin-top'] = tourHeight / 2 - 5;
-
-		$tourPointer.css(tourPointerPos);
-		$tourBox.css(tourPos).show();
-	};
-});
+define(["jquery"],function(e){e.fn.showTour=function(t){$target=e(this),$tourBox=e('<div class="user-tour">				<span class="user-tour-title">This is title</span>				<div class="user-tour-pointer"></div>				<div class="user-tour-pointer animation"></div>			</div>').appendTo("body"),$tourPointer=$tourBox.children(".user-tour-pointer");var n={},r={};targetOffset=$target.offset(),targetWidth=$target.innerWidth(),targetHeight=$target.innerHeight(),tourWidth=$tourBox.width(),tourHeight=$tourBox.height(),targetOffset.left+targetWidth+tourWidth-document.documentElement.scrollLeft>window.innerWidth?(n.left=targetOffset.left-tourWidth-15,r["margin-left"]=tourWidth+5):(n.left=targetOffset.left+targetWidth+15,r["margin-left"]=-15),n.top=targetOffset.top-(tourHeight-targetHeight)/2,r["margin-top"]=tourHeight/2-5,$tourPointer.css(r),$tourBox.css(n).show()}});
